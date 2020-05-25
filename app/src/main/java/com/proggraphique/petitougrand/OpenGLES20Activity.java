9b47f917423f98f17d85acecffc36b9591b1f2b4
martintinC <martines.capdevila@gmail.com>
2020-05-25 19:47:39 +0200

    tests boutons

--- app/src/main/java/com/proggraphique/petitougrand/OpenGLES20Activity.java
+++ app/src/main/java/com/proggraphique/petitougrand/OpenGLES20Activity.java
@@ -6,8 +6,12 @@ import android.os.Bundle;
 
 import android.app.Activity;
 import android.opengl.GLSurfaceView;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 /* Ce tutorial est issu d'un tutorial http://developer.android.com/training/graphics/opengl/index.html :
 openGLES.zip HelloOpenGLES20
@@ -18,6 +22,11 @@ public class OpenGLES20Activity extends Activity {
 
     // le conteneur View pour faire du rendu OpenGL
     private GLSurfaceView mGLView;
+
+
+
+
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -36,5 +45,36 @@ public class OpenGLES20Activity extends Activity {
         mGLView = new MyGLSurfaceView(this);
         /* Définition de View pour cette activité */
         setContentView(mGLView);
+
+
+
+
+       RelativeLayout ll = new RelativeLayout(this);
+
+        Button moins = new Button(this);
+        moins.setText("moins");
+
+        Button egal = new Button(this);
+        egal.setText("egal");
+
+        Button plus = new Button(this);
+        plus.setText("plus");
+
+        Button arreter = new Button(this);
+        arreter.setText("arreter");
+
+        ll.addView(moins);
+        ll.addView(egal);
+        ll.addView(plus);
+        ll.addView(arreter);
+
+        //LayoutParams lButtonParams = new RelativeLayout.LayoutParams( LayoutParams.WRAP_CONTENT , LayoutParams.WRAP_CONTENT );
+
+        this.addContentView(ll,new
+                RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT));
+
+
+
+
     }
 }
